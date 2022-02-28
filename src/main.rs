4ba4010b8ff0602b81bdb70c87ffa870e724f71c
AilurusRedPanda <1148648777@qq.com>
2022-02-28 17:44:18 +0800

    added feature for notes decoding

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use base64::encode;
+use base64::{decode, encode};
 use rpassword::read_password;
 use std::fs;
 use std::io::Write;
 
 enum Input {
+    Decode,
     Exit,
     Other,
 }
@@ -27,6 +28,7 @@ fn text_print_and_save(mut text: String) {
 
 fn handle_input(console_input: String) -> Input {
     match &console_input[..] {
+        "decode()" => Input::Decode,
         "exit()" => Input::Exit,
         _ => {
             text_print_and_save(console_input);
@@ -41,10 +43,22 @@ fn console_reader() -> Input {
     handle_input(console_input)
 }
 
+fn decode_notes() {
+    let notes = fs::read_to_string("./note.txt").unwrap();
+    let notes_list = notes.split('\n').collect::<Vec<&str>>();
+    let notes_string = notes_list
+        .iter()
+        .map(|note_text| String::from_utf8(decode(note_text).unwrap()).unwrap() + "\n")
+        .collect::<String>();
+    fs::write("decoded_notes.txt", notes_string).unwrap();
+}
+
 fn main() {
     loop {
-        if let Input::Exit = console_reader() {
-            break;
+        match console_reader() {
+            Input::Decode => decode_notes(),
+            Input::Exit => break,
+            _ => (),
         }
     }
 }
